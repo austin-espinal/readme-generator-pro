9d430f7324eda37750ac78ad922f217ad110f369
austin espinal <austinespi@gmail.com>
2021-12-13 18:36:32 -0500

    badge and link now displaying properly

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,86 +1,86 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'NONE') {
-    return '';
-  }
-  if (license === 'MIT') {
-    return `
-    ![badge](https://img.shields.io/badge/license-MIT-green)
-    `;
-  }
-  if (license === 'Apache 2.0') {
-    return `
-    ![badge](https://img.shields.io/badge/license-Apache%202.0-green)
-    `;
-  }
-  if (license === 'GNU General Public 3.0') {
-    return `
-    ![badge](https://img.shields.io/badge/license-GNU-General-Public-3.0-green)
-    `;
-  }
-  if (license === 'Creative Common Zero 1.0') {
-    return `
-    ![badge](https://img.shields.io/badge/license-Creative%20Common%20Zero%201.0-green)
-    `;
-  }
+if (license === 'NONE') {
+  return '';
+}
+if (license === 'MIT') {
+  return `
+  ![badge](https://img.shields.io/badge/license-MIT-green)
+  `;
+}
+if (license === 'Apache 2.0') {
+  return `
+  ![badge](https://img.shields.io/badge/license-Apache2.0-green)
+  `;
+}
+if (license === 'GNU General Public 3.0') {
+  return `
+  ![badge](https://img.shields.io/badge/license-GNUGeneralPublic3.0-green)
+  `;
+}
+if (license === 'Creative Common Zero 1.0') {
+  return `
+  ![badge](https://img.shields.io/badge/license-CreativeCommonZero1.0-green)
+  `;
+}
 };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === 'MIT') {
-    return `
-    [${license}](https://choosealicense.com/licenses/mit/)
-    `;
-  }
-  if (license === 'Apache 2.0') {
-    return `
-    [${license}](https://choosealicense.com/licenses/apache-2.0/)
-    `;
-  }
-  if (license === 'GNU General Public 3.0') {
-    return `
-    [${license}](https://choosealicense.com/licenses/gpl-3.0/)
-    `;
-  }
-  if (license === 'Creative Common Zero 1.0') {
-    return `
-    [${license}](https://choosealicense.com/licenses/cc0-1.0/)
-    `;
-  }
-  if (license === 'NONE') {
-    return '';
-  }
+if (license === 'MIT') {
+  return `
+  https://choosealicense.com/licenses/mit/
+  `;
+}
+if (license === 'Apache 2.0') {
+  return `
+  https://choosealicense.com/licenses/apache-2.0/
+  `;
+}
+if (license === 'GNU General Public 3.0') {
+  return `
+  https://choosealicense.com/licenses/gpl-3.0/
+  `;
+}
+if (license === 'Creative Common Zero 1.0') {
+  return `
+  https://choosealicense.com/licenses/cc0-1.0/
+  `;
+}
+if (license === 'NONE') {
+  return '';
+}
 };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === 'NONE') {
-    return '';
-  }
-  return `
-  ## License  
-    
+if (license === 'NONE') {
+  return '';
+}
+return `
+## License  
+  
 ${renderLicenseLink(license)}  
 The link to the license can be found above  
-  `;
+`;
 };
 
 //creates license section link whether a license is chosen
 function tableLink(license) {
-  if (license === 'NONE') {
-    return '';
-  }
-  return '* [License](#license)';
+if (license === 'NONE') {
+  return '';
+}
+return '* [License](#license)';
 };
 
 // TODO: Create a function to generate markdown for README
 module.exports = data => {
-  //destructured inquirer data
-  const { title, description, ...other } = data
-  return `
+//destructured inquirer data
+const { title, description, ...other } = data
+return `
 # ${title}
 ${renderLicenseBadge(other.license)}
 
